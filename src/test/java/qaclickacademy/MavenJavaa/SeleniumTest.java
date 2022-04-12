282ab7ac498b18c0d025b085d50e26351dc0a606
Sourav Rudra <souravrudra1234@gmail.com>
2022-04-12 23:25:35 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJavaa/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.MavenJavaa;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+	}
+}
